f92da25d28990ca01ed57dcb83d277c30ed21594
ibarhimnajoh <iprahimnajoh@gmail.com>
2026-07-07 10:05:26 +0300

    Update include components

--- includes/db.php
+++ includes/db.php
@@ -1,14 +1,27 @@
 <?php
+
+// Dejinta xogta lagu xirayo database-ka.
 $host = "localhost";
 $dbname = "livestock_system";
 $username = "root";
-$password = ""; // Badala hadii aad password u samaysay MySQL-kaaga
+$password = ""; // Haddii MySQL leeyahay password, halkan ku qor.
 
+// Isku day in la sameeyo xiriirka database-ka.
 try {
+
+    // Abuurista PDO Database Connection.
     $pdo = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8", $username, $password);
+
+    // Soo bandhig qaladka haddii uu dhaco.
     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+    // Xogta kasoo baxda database-ka ha noqoto Associative Array.
     $pdo->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
+
 } catch (PDOException $e) {
+
+    // Haddii xiriirku fashilmo, muuji fariinta qaladka.
     die("Database Connection Failed: " . $e->getMessage());
 }
+
 ?>
